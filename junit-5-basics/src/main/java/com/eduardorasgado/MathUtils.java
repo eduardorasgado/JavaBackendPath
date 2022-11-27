2cd9763367e85a6b809bfe576390012294f95e22
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2022-11-27 00:07:50 -0600

    creating my first testing class

---
+++ junit-5-basics/src/main/java/com/eduardorasgado/MathUtils.java
@@ -0,0 +1,12 @@
+package com.eduardorasgado;
+
+public class MathUtils {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public double computeCircleArea(double radious) {
+		return Math.PI * Math.pow(radious, 2);
+	}
+}
---
+++ junit-5-basics/src/test/java/com/eduardorasgado/MathUtilsTest.java
@@ -0,0 +1,28 @@
+package com.eduardorasgado;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void testAdd() {
+		MathUtils mathUtils = new MathUtils();
+		
+		int expected = 2;
+		int actual = mathUtils.add(1, 1);
+
+		assertEquals(expected, actual, "The add method should add two numbers");
+	}
+
+	@Test
+	void testComputeCircleArea() {
+		MathUtils mathUtils = new MathUtils();
+		
+		double expected = 201.06192982974676;
+		double actual = mathUtils.computeCircleArea(8);
+		
+		assertEquals(expected, actual, "Should return right circle area");
+	}
+}
