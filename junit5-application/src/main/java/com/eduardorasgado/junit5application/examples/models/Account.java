f0cbcab2911b66e1af9fc04b82d077e54910b584
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2022-11-28 21:24:28 -0600

    withdraw and deposit testing

--- junit5-application/src/main/java/com/eduardorasgado/junit5application/examples/models/Account.java
+++ junit5-application/src/main/java/com/eduardorasgado/junit5application/examples/models/Account.java
@@ -1,6 +1,7 @@
 package com.eduardorasgado.junit5application.examples.models;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Account {
     private String person;
@@ -11,6 +12,17 @@ public class Account {
         this.person = person;
     }
 
+    public void withdraw(BigDecimal amount) {
+        setBalance(scaleAmount(getBalance().subtract(amount)));
+    }
+
+    public void deposit(BigDecimal amount) {
+        setBalance(scaleAmount(getBalance().add(amount)));
+    }
+
+    private BigDecimal scaleAmount(BigDecimal amount) {
+        return amount.setScale(5, RoundingMode.FLOOR);
+    }
     public String getPerson() {
         return person;
     }
@@ -25,5 +37,13 @@ public class Account {
 
     public void setBalance(BigDecimal balance) {
         this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return getPerson().equals(account.getPerson()) && getBalance().equals(account.getBalance());
     }
 }
--- junit5-application/src/test/java/com/eduardorasgado/junit5application/examples/models/AccountTest.java
+++ junit5-application/src/test/java/com/eduardorasgado/junit5application/examples/models/AccountTest.java
@@ -15,6 +15,7 @@ class AccountTest {
         String expected = "Andy";
         String actual = account.getPerson();
 
+        assertNotNull(actual);
         assertEquals(expected, actual);
     }
 
@@ -22,8 +23,31 @@ class AccountTest {
     void testBalance() {
         Account account = new Account("Andy", new BigDecimal("1000.1234"));
 
+        assertNotNull(account.getBalance());
         assertEquals(1000.1234, account.getBalance().doubleValue(), "Expect balance");
         assertFalse(account.getBalance().compareTo(BigDecimal.ZERO) < 0);
+    }
+
+    @Test
+    void testWithdraw() {
+        Account account = new Account("Andy Ruiz", new BigDecimal("11250.45009"));
+        account.withdraw(new BigDecimal(125.20));
+
+        BigDecimal expected = new BigDecimal("11125.25008");
+        BigDecimal actual = account.getBalance();
+
+        assertEquals(expected, actual, "withdraw money from account");
+    }
+
+    @Test
+    void testDeposit() {
+        Account account = new Account("Andy Ruiz", new BigDecimal("11250.45009"));
+        account.deposit(new BigDecimal(125.78));
+
+        BigDecimal expected = new BigDecimal("11376.23009");
+        BigDecimal actual = account.getBalance();
+
+        assertEquals(expected, actual, "deposit money to account");
     }
 
     @Test
@@ -31,6 +55,6 @@ class AccountTest {
         Account account = new Account("John Doe", new BigDecimal("8900.9997"));
         Account account2 = new Account("John Doe", new BigDecimal("8900.9997"));
 
-        assertNotEquals(account, account2, "Different objects");
+        assertEquals(account, account2, "Objects compared by value");
     }
 }
