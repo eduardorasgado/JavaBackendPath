a7e054e73acd135a298d8f1c6bd963ee82f4f7aa
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2022-12-03 21:59:50 -0600

    testing for iterables, arrays and throwing exceptions

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -2,6 +2,9 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(ReplaceCamelCase.class)
@@ -36,7 +39,52 @@ class DemoUtilsTest {
         assertNull(demoUtils.checkNull(str1), "Object should be null");
         assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
     }
-/*
+
+    @Test
+    void testSameAndNotSame() {
+        String str = "luv2Code";
+
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to the same object");
+        assertNotSame(str, demoUtils.getAcademy(), "Objects should not refer to same object");
+    }
+
+    @Test
+    void testTrueAndFalse() {
+        int gradeOne = 10;
+        int gradeTwo = 5;
+
+        assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
+        assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
+    }
+
+    @Test
+    void testArrayEquals() {
+        String[] stringArray = {"A", "B", "C"};
+
+        assertArrayEquals(stringArray,demoUtils.getFirstThreeLettersOfAlphabet(), "Arrays should be equals");
+    }
+
+    @Test
+    void testIterableEquals() {
+        List<String> expectedIterable = List.of("luv", "2", "code");
+
+        assertIterableEquals(expectedIterable, demoUtils.getAcademyInList(), "Expected iterable should be  same as actual iterable");
+    }
+
+    @Test
+    void testLinesMatch() {
+        List<String> expectedIterable = List.of("luv", "\\d+", "code");
+
+        assertLinesMatch(expectedIterable, demoUtils.getAcademyInList(), "Lines should match");
+    }
+
+    @Test
+    void testThrowsAndDoesNotThrow() {
+        assertThrows(Exception.class, () -> demoUtils.throwException(-1), "Should throw exception");
+        assertDoesNotThrow(() -> demoUtils.throwException(5), "Should not throw exception");
+    }
+
+    /*
     @AfterEach
     void tearDownAfterEach(TestReporter testReporter) {
         testReporter.publishEntry("@AfterEach executes before the execution of each test method");
