e100ca1abdd33de697f945bcd71ee4c16f929182
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2022-12-04 23:17:27 -0600

    getting started using junit with spring boot

---
+++ 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -0,0 +1,29 @@
+package com.luv2code.test;
+
+
+import com.luv2code.component.MvcTestingExampleApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class ApplicationExampleTest {
+
+    private static int count = 0;
+
+    @Value("${info.app.name}")
+    private String appInfo;
+
+    @Value("${info.app.description}")
+    private String appDescription;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+    @Value("${info.school.name}")
+    private String appSchoolName;
+
+    @Test
+    void basicTest() {
+    }
+}
