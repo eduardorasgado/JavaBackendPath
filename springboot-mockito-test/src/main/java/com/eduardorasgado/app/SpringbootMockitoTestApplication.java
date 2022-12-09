b67844ee1474cd1538058f1e9e34b89c6ee82748
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2022-12-09 14:39:35 -0600

    new spring boot project created to learn testing over spring boot applications

---
+++ springboot-mockito-test/src/main/java/com/eduardorasgado/app/SpringbootMockitoTestApplication.java
@@ -0,0 +1,13 @@
+package com.eduardorasgado.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMockitoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMockitoTestApplication.class, args);
+	}
+
+}
---
+++ springboot-mockito-test/src/test/java/com/eduardorasgado/app/SpringbootMockitoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardorasgado.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMockitoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
