191f9a88ec41399693d903636b8f0e3726b42f4b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2022-12-12 17:05:11 -0600

    swagger config class created

---
+++ springboot-mockito-test/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+-- ACCOUNTS
+INSERT INTO accounts (name, balance) VALUES ('Adam Smith', 1000);
+INSERT INTO accounts (name, balance) VALUES ('John Finn' , 2000);
+
+-- BANKS
+INSERT INTO banks (name, total_transfers) VALUES ('Nu Bank', 0);
+
