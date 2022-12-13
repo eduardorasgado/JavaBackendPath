b676ef176ae76177e19697b9b7b6d177dd166730
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2022-12-13 17:51:55 -0600

    first approach to test account controller transfer method

--- springboot-mockito-test/src/test/java/com/eduardorasgado/app/controllers/AccountControllerTest.java
+++ springboot-mockito-test/src/test/java/com/eduardorasgado/app/controllers/AccountControllerTest.java
@@ -1,7 +1,11 @@
 package com.eduardorasgado.app.controllers;
 
 import com.eduardorasgado.app.AccountTestData;
+import com.eduardorasgado.app.payloads.dtos.accounts.request.TransactionRequestDto;
 import com.eduardorasgado.app.services.AccountService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,6 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
@@ -27,21 +34,62 @@ class AccountControllerTest {
     @MockBean
     private AccountService accountService;
 
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
     @Test
-    void detail() throws Exception {
+    void testDetail() throws Exception {
         // Given
         when(accountService.findById(1L)).thenReturn(AccountTestData.getNewAccount001().orElseThrow());
 
         // When
         mvc.perform(MockMvcRequestBuilders
                     .get("/api/accounts/1")
-                    .contentType(MediaType.APPLICATION_JSON))
+                    .contentType(MediaType.APPLICATION_JSON)
+                )
                 // Then
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                // $. at jsonPath indicates the root of the json object
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Adam Smith"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.balance").value(1000));
 
         verify(accountService).findById(1L);
+    }
+
+    @Test
+    void testTransfer() throws Exception {
+        TransactionRequestDto transactionRequestDto = new TransactionRequestDto();
+        transactionRequestDto.setOriginAccountId(1L);
+        transactionRequestDto.setDestinationAccountId(2L);
+        transactionRequestDto.setBankId(1L);
+        transactionRequestDto.setAmount(new BigDecimal("500"));
+
+        mvc.perform(MockMvcRequestBuilders
+                    .post("/api/accounts/transfer")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(transactionRequestDto))
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.date").value(LocalDate.now().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("OK"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message")
+                                                    .value("Transfer was successfully performed"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.transaction.originAccountId")
+                                                    .value(transactionRequestDto.getOriginAccountId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.transaction.destinationAccountId")
+                                                    .value(transactionRequestDto.getDestinationAccountId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.transaction.bankId")
+                                                    .value(transactionRequestDto.getBankId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.transaction.amount")
+                                                    .value(transactionRequestDto.getAmount()));
+
+
+
     }
 }
