f518aed4ec6001e49800f4918d9aced0477f2d26
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2022-12-16 22:29:15 -0600

    integration testing for list all with jsonPath and consumeWith

--- springboot-mockito-test/src/test/java/com/eduardorasgado/app/controllers/AccountControllerRestServiceIntegrationTest.java
+++ springboot-mockito-test/src/test/java/com/eduardorasgado/app/controllers/AccountControllerRestServiceIntegrationTest.java
@@ -4,7 +4,9 @@ import com.eduardorasgado.app.AccountTestData;
 import com.eduardorasgado.app.models.Account;
 import com.eduardorasgado.app.payloads.dtos.accounts.request.TransactionRequestDto;
 import com.eduardorasgado.app.payloads.dtos.accounts.response.AccountResponseDto;
+import com.eduardorasgado.app.payloads.dtos.accounts.response.ListAllAccountsResponseDto;
 import com.eduardorasgado.app.payloads.mappers.accounts.response.AccountResponseDtoMapper;
+import com.eduardorasgado.app.payloads.mappers.accounts.response.ListAllAccountsResponseDtoMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,7 +21,9 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,7 +48,7 @@ class AccountControllerRestServiceIntegrationTest {
     private String serviceUrl;
 
     @Test
-    @Order(3)
+    @Order(10)
     void testTransfer() throws JsonProcessingException {
         // Given
         TransactionRequestDto dto = new TransactionRequestDto();
@@ -152,6 +156,73 @@ class AccountControllerRestServiceIntegrationTest {
                     assertEquals(accountToFetch.getId(), accountDto.getId());
                     assertEquals(accountToFetch.getName(), accountDto.getName());
                     assertEquals(0, accountToFetch.getBalance().compareTo(accountDto.getBalance()));
+                });
+    }
+
+    @Test
+    @Order(3)
+    void testListAllJsonPath() {
+        ListAllAccountsResponseDto responseDto = ListAllAccountsResponseDtoMapper.mapModelToDto(Arrays.asList(
+                AccountTestData.getNewAccount001().orElseThrow(),
+                AccountTestData.getNewAccount002().orElseThrow()
+        ), new ListAllAccountsResponseDto());
+
+        webClient.get().uri("/api/accounts")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+
+                .jsonPath("$.accounts[0].id").isEqualTo(responseDto.getAccounts().get(0).getId())
+                .jsonPath("$.accounts[0].name").isEqualTo(responseDto.getAccounts().get(0).getName())
+                .jsonPath("$.accounts[0].balance").isEqualTo(responseDto.getAccounts().get(0).getBalance().doubleValue())
+
+                .jsonPath("$.accounts[1].id").isEqualTo(responseDto.getAccounts().get(1).getId())
+                .jsonPath("$.accounts[1].name").isEqualTo(responseDto.getAccounts().get(1).getName())
+                .jsonPath("$.accounts[1].balance").isEqualTo(responseDto.getAccounts().get(1).getBalance().doubleValue())
+
+                .jsonPath("$.accounts").isArray()
+                .jsonPath("$.accounts").value(Matchers.hasSize(2));
+    }
+
+    @Test
+    @Order(4)
+    void testListAllConsumeWith() {
+        ListAllAccountsResponseDto expectedResponseDto = ListAllAccountsResponseDtoMapper.mapModelToDto(Arrays.asList(
+                AccountTestData.getNewAccount001().orElseThrow(),
+                AccountTestData.getNewAccount002().orElseThrow()
+        ), new ListAllAccountsResponseDto());
+
+        webClient.get().uri("/api/accounts")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                // if we were returning  a List<AccountDto> we could be using the next statement
+                //.expectBodyList(AccountResponseDto.class)
+                //.consumeWith(response -> {
+                //    List<AccountResponseDto> accounts = response.getResponseBody();
+                //});
+                .expectBody(ListAllAccountsResponseDto.class)
+
+                .consumeWith(response -> {
+                    ListAllAccountsResponseDto actualResponseDto = response.getResponseBody();
+
+                    assertNotNull(actualResponseDto);
+
+                    List<ListAllAccountsResponseDto.AccountResponseDto> expectedDtoList = expectedResponseDto.getAccounts();
+                    List<ListAllAccountsResponseDto.AccountResponseDto> actualDtoList = actualResponseDto.getAccounts();
+
+                    assertNotNull(actualDtoList);
+                    assertFalse(actualDtoList.isEmpty());
+                    assertEquals(expectedDtoList.size(), actualDtoList.size());
+
+                    assertEquals(expectedDtoList.get(0).getId(), actualDtoList.get(0).getId());
+                    assertEquals(expectedDtoList.get(0).getName(), actualDtoList.get(0).getName());
+                    assertEquals(0, expectedDtoList.get(0).getBalance().compareTo(actualDtoList.get(0).getBalance()));
+
+                    assertEquals(expectedDtoList.get(1).getId(), actualDtoList.get(1).getId());
+                    assertEquals(expectedDtoList.get(1).getName(), actualDtoList.get(1).getName());
+                    assertEquals(0, expectedDtoList.get(1).getBalance().compareTo(actualDtoList.get(1).getBalance()));
                 });
     }
 }
