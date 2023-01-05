2255fa6a44c36f6364063b0d20af33e78ed5cf01
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-01-05 01:40:38 -0600

    performance props to kafka producer

--- apache-kafka-with-java-spring-aws-course/vanilla-kafka-integration/src/main/java/com/eduardocode/producers/CheetosProducer.java
+++ apache-kafka-with-java-spring-aws-course/vanilla-kafka-integration/src/main/java/com/eduardocode/producers/CheetosProducer.java
@@ -2,6 +2,7 @@ package com.eduardocode.producers;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
@@ -13,23 +14,28 @@ public class CheetosProducer {
     private final static Logger logger = LoggerFactory.getLogger(CheetosProducer.class);
 
     public static void main(String[] args) {
-        String mode = "[SYNC 5] ";
-        Properties props = new Properties();
+        String mode = "[ASYNC M -4s] ";
 
-        props.put("bootstrap.servers", "localhost:9092"); //kafka broker
-        props.put("acks", "1"); // 0, 1, all
-        props.put("key.serializer", StringSerializer.class);
-        props.put("value.serializer", StringSerializer.class);
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); //kafka broker
+        props.put(ProducerConfig.ACKS_CONFIG, "1"); // 0, 1, all
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        // performance props
+        props.put(ProducerConfig.LINGER_MS_CONFIG, "4");
+        //props.put(ProducerConfig.LINGER_MS_CONFIG, "4000");
+        //props.put(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(16 * 1024));
 
         long startTime = System.currentTimeMillis();
 
-        try(Producer<String, String> producer = new KafkaProducer<String, String>(props)) {
-            for (int i = 0; i < 100000; i++) {
+        try(Producer<String, String> producer = new KafkaProducer<>(props)) {
+            for (int i = 0; i < 1000000; i++) {
                 String index = String.valueOf(i);
 
                 producer.send(new ProducerRecord<String, String>(
                         "vanilla-topic",
-                        mode + "vanilla-key-" + index,
+                        mode + "vanilla-key",
                         "vanilla-value-" + index)
                 );
             }
--- apache-kafka-with-java-spring-aws-course/vanilla-kafka-integration/src/main/java/com/eduardocode/producers/SyncCheetosProducer.java
+++ apache-kafka-with-java-spring-aws-course/vanilla-kafka-integration/src/main/java/com/eduardocode/producers/SyncCheetosProducer.java
@@ -2,6 +2,7 @@ package com.eduardocode.producers;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
@@ -14,7 +15,7 @@ public class SyncCheetosProducer {
     private final static Logger logger = LoggerFactory.getLogger(SyncCheetosProducer.class);
 
     public static void main(String[] args) {
-        String mode = "[SYNC]";
+        String mode = "[SYNC -1ms]";
         Properties props = new Properties();
 
         props.put("bootstrap.servers", "localhost:9092"); //kafka broker
@@ -22,14 +23,19 @@ public class SyncCheetosProducer {
         props.put("key.serializer", StringSerializer.class);
         props.put("value.serializer", StringSerializer.class);
 
-        try(Producer<String, String> producer = new KafkaProducer<String, String>(props)) {
+        props.put(ProducerConfig.LINGER_MS_CONFIG, "2");
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(64 * 1024)); // 16 * 1024 = 16kb
+
+        long startTime = System.currentTimeMillis();
+
+        try(Producer<String, String> producer = new KafkaProducer<>(props)) {
             for (int i = 0; i < 10000; i++) {
                 String index = String.valueOf(i);
 
                 producer.send(new ProducerRecord<String, String>(
                         "vanilla-topic",
-                        mode + "vanilla-key-" + index,
-                        "vanilla-value-" + index)
+                        mode + "vanilla-key",
+                        index)
                 ).get();
             }
 
@@ -37,5 +43,7 @@ public class SyncCheetosProducer {
         } catch (ExecutionException | InterruptedException e) {
             logger.error("[MESSAGE PRODUCER INTERRUPTED]", e);
         }
+
+        logger.info("Processing time = {} ms ", System.currentTimeMillis() - startTime);
     }
 }
