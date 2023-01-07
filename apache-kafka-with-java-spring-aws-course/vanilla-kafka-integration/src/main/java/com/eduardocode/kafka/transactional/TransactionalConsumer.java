6649ea5e3299592b18ab91fbb18a6a6242e64fcd
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-01-07 02:41:47 -0600

    creating transactional consumer and producer

---
+++ apache-kafka-with-java-spring-aws-course/vanilla-kafka-integration/src/main/java/com/eduardocode/kafka/transactional/TransactionalConsumer.java
@@ -0,0 +1,44 @@
+package com.eduardocode.kafka.transactional;
+
+import com.eduardocode.kafka.consumers.CheetosConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class TransactionalConsumer {
+
+    private static final String VANILLA_TOPIC = "vanilla-topic";
+
+    private static final Logger logger = LoggerFactory.getLogger(CheetosConsumer.class);
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "vanilla-group");
+        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, Integer.toString(1000));
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
+
+        props.setProperty(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+
+        try(Consumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(List.of(VANILLA_TOPIC));
+
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
+
+                for (ConsumerRecord<String, String> record : consumerRecords) {
+                    logger.info("Offset = {}, Partition = {}, Key = {}, Value = {}",
+                            record.offset(), record.partition(), record.key(), record.value());
+                }
+            }
+        }
+    }
+}
---
+++ apache-kafka-with-java-spring-aws-course/vanilla-kafka-integration/src/main/java/com/eduardocode/kafka/transactional/TransactionalProducer.java
@@ -0,0 +1,64 @@
+package com.eduardocode.kafka.transactional;
+
+import com.eduardocode.kafka.producers.CheetosProducer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class TransactionalProducer {
+
+    private final static Logger logger = LoggerFactory.getLogger(CheetosProducer.class);
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); //kafka broker
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        // transactional feature key config -------------------
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "vanilla-producer-id");
+        // -------------------
+
+        // performance props
+        props.put(ProducerConfig.LINGER_MS_CONFIG, "4");
+
+        long startTime = System.currentTimeMillis();
+
+        try(Producer<String, String> producer = new KafkaProducer<>(props)) {
+            try {
+                producer.initTransactions();
+                producer.beginTransaction();
+
+                for (int i = 0; i < 100000; i++) {
+                    String index = String.valueOf(i);
+
+                    producer.send(new ProducerRecord<String, String>(
+                            "vanilla-topic",
+                            "vanilla-key",
+                            index)
+                    );
+
+//                    if(i >= 90000) {
+//                        throw new Exception("Unexpected exception");
+//                    }
+                }
+
+                producer.commitTransaction();
+                producer.flush();
+            } catch (Exception ex) {
+                logger.error("Error ", ex);
+
+                producer.abortTransaction();
+            }
+        }
+
+        logger.info("Processing time = {} ms ", System.currentTimeMillis() - startTime);
+    }
+}
