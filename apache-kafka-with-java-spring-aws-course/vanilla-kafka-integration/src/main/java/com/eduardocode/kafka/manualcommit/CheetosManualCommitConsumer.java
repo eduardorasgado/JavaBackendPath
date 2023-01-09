ccfbae96bd798e7482a20361f3160631822cbf7c
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-01-09 00:13:25 -0600

    creating a consumer based on manual commits

--- apache-kafka-with-java-spring-aws-course/vanilla-kafka-integration/src/main/java/com/eduardocode/kafka/manualcommit/CheetosManualCommitConsumer.java
+++ apache-kafka-with-java-spring-aws-course/vanilla-kafka-integration/src/main/java/com/eduardocode/kafka/manualcommit/CheetosManualCommitConsumer.java
@@ -1,7 +1,6 @@
-package com.eduardocode.kafka.assignseek;
+package com.eduardocode.kafka.manualcommit;
 
 import org.apache.kafka.clients.consumer.*;
-import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,45 +9,41 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 
-public class CheetosAssignSeekConsumer {
+public class CheetosManualCommitConsumer {
 
     private static final String VANILLA_TOPIC = "vanilla-topic";
 
-    private static final int TOPIC_PARTITION = 0; // where consumer previously allocated producers messages.
-
-    private static final int TOPIC_OFFSET = 50;
-
-    private static final int POLL_DURATION_MILLIS = 100;
-
-    private static final Logger logger = LoggerFactory.getLogger(CheetosAssignSeekConsumer.class);
+    private static final Logger logger = LoggerFactory.getLogger(CheetosManualCommitConsumer.class);
 
     public static void main(String[] args) {
         Properties props = new Properties();
 
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "vanilla-group");
-        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-        props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, Integer.toString(1000));
         props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
 
         props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        try(Consumer<String, String> consumer = new KafkaConsumer<>(props)) {
-            TopicPartition topicPartition = new TopicPartition(VANILLA_TOPIC, TOPIC_PARTITION);
+        //props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        //props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, Integer.toString(1000));
+        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
-            // assign: to assign this consumer an specific partition to read from
-            consumer.assign(List.of(topicPartition));
-            // seek: to start reading on a certain partition at selected offset
-            consumer.seek(topicPartition, TOPIC_OFFSET);
+        try(Consumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(List.of(VANILLA_TOPIC));
 
             while (true) {
-                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(POLL_DURATION_MILLIS));
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
 
                 for (ConsumerRecord<String, String> record : consumerRecords) {
                     logger.info("Offset = {}, Partition = {}, Key = {}, Value = {}",
                             record.offset(), record.partition(), record.key(), record.value());
+
+                    consumer.commitSync();
                 }
+                // we can either commit where our logic requires to do it, probably we want to commit on every record read
+                // or at the end of the total reading
+                //consumer.commitSync();
             }
         }
     }
---
+++ apache-kafka-with-java-spring-aws-course/vanilla-kafka-integration/src/main/java/com/eduardocode/kafka/manualcommit/CheetosManualCommitProducer.java
@@ -0,0 +1,37 @@
+package com.eduardocode.kafka.manualcommit;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+// Consumer: partition 0
+public class CheetosManualCommitProducer {
+    private final static Logger logger = LoggerFactory.getLogger(CheetosManualCommitProducer.class);
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); //kafka broker
+        props.put(ProducerConfig.ACKS_CONFIG, "all"); // 0, 1, all
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
+        props.put(ProducerConfig.LINGER_MS_CONFIG, "4");
+
+        long startTime = System.currentTimeMillis();
+
+        try(Producer<String, String> producer = new KafkaProducer<>(props)) {
+            for (int i = 0; i < 100; i++) {
+                producer.send(new ProducerRecord<>("vanilla-topic", "vanilla-manualcommit-key", String.valueOf(i)));
+            }
+
+            producer.flush();
+        }
+
+        logger.info("Processing time = {} ms ", System.currentTimeMillis() - startTime);
+    }
+}
