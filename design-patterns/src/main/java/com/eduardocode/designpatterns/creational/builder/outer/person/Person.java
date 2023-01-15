0c1ca980e82857c09e17c8b2388a3e0d6c2191b3
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-01-14 21:37:46 -0600

    creating two examples of builder design pattern by using outer and inner builder class related to the built object

---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/creational/builder/outer/person/Person.java
@@ -0,0 +1,47 @@
+package com.eduardocode.designpatterns.creational.builder.outer.person;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String address;
+
+    public Person() {}
+
+    public String getInformation() {
+        return String.format("Information: full name> %s %s, age> %d, address>%s",
+                getFirstName(), getLastName(), getAge(), getAddress());
+    }
+
+    private String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/creational/builder/outer/person/PersonDemo.java
@@ -0,0 +1,14 @@
+package com.eduardocode.designpatterns.creational.builder.outer.person;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person person = new PersonBuilder()
+                .setFirstName("Eduardo")
+                .setLastName("Rasgado")
+                .setAge(28)
+                .setAddress("Av example #1234, Mexico City")
+                .build();
+
+        System.out.println(person.getInformation());
+    }
+}
