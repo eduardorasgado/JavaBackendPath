32da5941ba2cdc03288cc5c385726bcf99a453b8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-01-16 21:09:02 -0600

    document example was created for flyweight dp

---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/structural/flyweight/document/Document.java
@@ -0,0 +1,19 @@
+package com.eduardocode.designpatterns.structural.flyweight.document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Document {
+    private List<Font> fonts;
+
+    public Document() {
+        this.fonts = new ArrayList<>();
+    }
+
+    public void addText(String fontName, int fontSize, String text) {
+        Font font = FontFactory.getFont(fontName, fontSize);
+        fonts.add(font);
+
+        System.out.println("Added text: " + text + " with font: " + fontName + "-" + fontSize);
+    }
+}
---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/structural/flyweight/document/Font.java
@@ -0,0 +1,13 @@
+package com.eduardocode.designpatterns.structural.flyweight.document;
+
+public class Font {
+    public String fontName;
+    public int fontSize;
+
+    public Font(String fontName, int fontSize) {
+        System.out.println("New font stored in the JVM");
+
+        this.fontName = fontName;
+        this.fontSize = fontSize;
+    }
+}
