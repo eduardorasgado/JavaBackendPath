d4469bc9b7a88a1df912297aafa46186427b368c
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-01-19 23:57:15 -0600

    creating a simple example for memento dp encapsulating the originator state

---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/behavioral/memento/simpleprotected/ProtectedMementoDemo.java
@@ -0,0 +1,33 @@
+package com.eduardocode.designpatterns.behavioral.memento.simpleprotected;
+
+public class ProtectedMementoDemo {
+
+    public static void main(String[] args) {
+        Originator originator = new Originator();
+        CareTaker careTaker = new CareTaker();
+
+        originator.setState("State 1");
+        careTaker.add(originator);
+
+        originator.doSomethingPublic();
+
+        originator.setState("State 2");
+        careTaker.add(originator);
+
+        originator.doSomethingPublic();
+
+        originator.setState("State 3");
+        careTaker.add(originator);
+
+        originator.doSomethingPublic();
+
+        originator.setState("State 4");
+        careTaker.add(originator);
+
+        originator.doSomethingPublic();
+
+        careTaker.restore(originator, 1);
+
+        originator.doSomethingPublic();
+    }
+}
---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/behavioral/memento/simpleprotected/PublicMemento.java
@@ -0,0 +1,4 @@
+package com.eduardocode.designpatterns.behavioral.memento.simpleprotected;
+
+public interface PublicMemento {
+}
