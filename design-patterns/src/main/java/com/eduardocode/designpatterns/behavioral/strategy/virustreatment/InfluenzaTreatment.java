1ebdc50ca6dc6356d01e418478e70162702aae8b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-01-22 02:07:04 -0600

    virus treatment example for strategy dp

---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/behavioral/strategy/virustreatment/InfluenzaTreatment.java
@@ -0,0 +1,9 @@
+package com.eduardocode.designpatterns.behavioral.strategy.virustreatment;
+
+public class InfluenzaTreatment extends Treatment {
+
+    @Override
+    void treat(Patient patient) {
+        System.out.println("Applying Influenza vaccine to patient " + patient.getName() + " with symptom: " + patient.getSymptom());
+    }
+}
---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/behavioral/strategy/virustreatment/Treatment.java
@@ -0,0 +1,10 @@
+package com.eduardocode.designpatterns.behavioral.strategy.virustreatment;
+
+public abstract class Treatment {
+
+    public boolean equals(Treatment treatment) {
+        return this.getClass().getSimpleName().equals(treatment.getClass().getSimpleName());
+    }
+
+    abstract void treat(Patient patient);
+}
