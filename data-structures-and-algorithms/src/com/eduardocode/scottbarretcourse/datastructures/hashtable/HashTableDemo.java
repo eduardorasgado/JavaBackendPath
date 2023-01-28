c3c4274951d4f5a6b9a371b67c37f3c843c3b56e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-01-27 23:00:43 -0600

    getting started with hash tables with a class, node, print method and constructor

---
+++ data-structures-and-algorithms/src/com/eduardocode/scottbarretcourse/datastructures/hashtable/HashTableDemo.java
@@ -0,0 +1,13 @@
+package com.eduardocode.scottbarretcourse.datastructures.hashtable;
+
+public class HashTableDemo {
+    private static HashTable ht = new HashTable();
+    public static void main(String[] args) {
+        constructor();
+    }
+
+    private static void constructor() {
+        ht.print();
+    }
+
+}
