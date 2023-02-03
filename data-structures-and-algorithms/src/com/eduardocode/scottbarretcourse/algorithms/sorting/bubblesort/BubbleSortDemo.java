f77458973eab938685423bd116f6430abfa6b7c8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-02-03 02:11:33 -0600

    bubble sort algorithm slightly changed

--- data-structures-and-algorithms/src/com/eduardocode/scottbarretcourse/algorithms/sorting/bubblesort/BubbleSortDemo.java
+++ data-structures-and-algorithms/src/com/eduardocode/scottbarretcourse/algorithms/sorting/bubblesort/BubbleSortDemo.java
@@ -10,8 +10,8 @@ public class BubbleSortDemo {
     }
 
     private static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 if(arr[j] > arr[j + 1]) {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
