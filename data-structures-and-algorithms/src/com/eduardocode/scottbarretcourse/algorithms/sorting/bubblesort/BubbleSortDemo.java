f558c55d1f5651bd9f6a3b4693a3951a6f676ea5
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-02-03 02:02:30 -0600

    getting started with sorting algorithms with bubble sort

---
+++ data-structures-and-algorithms/src/com/eduardocode/scottbarretcourse/algorithms/sorting/bubblesort/BubbleSortDemo.java
@@ -0,0 +1,30 @@
+package com.eduardocode.scottbarretcourse.algorithms.sorting.bubblesort;
+
+public class BubbleSortDemo {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 6, 5, 1, 3};
+        printArr(arr);
+        bubbleSort(arr);
+        printArr(arr);
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    private static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print( arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
