a66b678517e662ebc29c504e92830fbe168ff650
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-02-04 01:51:07 -0600

    insertion sort rewrote to match actual algorithm

--- data-structures-and-algorithms/src/com/eduardocode/scottbarretcourse/algorithms/sorting/insertionsort/InsertionSort.java
+++ data-structures-and-algorithms/src/com/eduardocode/scottbarretcourse/algorithms/sorting/insertionsort/InsertionSort.java
@@ -18,14 +18,13 @@ public class InsertionSort extends ArraySortingAlgorithm {
     @Override
     protected void sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
 
-            for (int j = i; j > 0; j--) {
-                if(arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                }
+            while (j >= 0 && temp < arr[j]) {
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                j--;
             }
         }
     }
