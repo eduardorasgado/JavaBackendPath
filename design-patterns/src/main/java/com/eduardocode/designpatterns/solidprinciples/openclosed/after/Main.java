efe4c518ae1c44a4cbce8ef8f1282948bb819d99
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-02-08 00:50:13 -0600

    OCP example created: before and after pkgs

---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/solidprinciples/openclosed/after/Main.java
@@ -0,0 +1,8 @@
+package com.eduardocode.designpatterns.solidprinciples.openclosed.after;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ISPSubscriber subscriber = new ISPSubscriber();
+    }
+}
---
+++ design-patterns/src/main/java/com/eduardocode/designpatterns/solidprinciples/openclosed/before/Main.java
@@ -0,0 +1,8 @@
+package com.eduardocode.designpatterns.solidprinciples.openclosed.before;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
