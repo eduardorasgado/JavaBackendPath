cac9ed4891627337ba0715cb009ce327516ca697
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-02-12 21:31:30 -0600

    radio device added to remote control example for bridge dp

--- design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/bridge/remotecontrol/RemoteControlDemo.java
+++ design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/bridge/remotecontrol/RemoteControlDemo.java
@@ -4,6 +4,7 @@ public class RemoteControlDemo {
 
     public static void main(String[] args) {
         testDevice(new Tv());
+        testDevice(new Radio());
     }
 
     public static void testDevice(Device device) {
