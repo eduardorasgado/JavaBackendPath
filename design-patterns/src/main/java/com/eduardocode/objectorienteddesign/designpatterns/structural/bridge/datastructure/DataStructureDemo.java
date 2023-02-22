7e804c064db68a68c2fc22d8c20d14e1a04b278e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-02-22 01:11:28 -0600

    datastructure example finished for bridge dp

--- design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/bridge/datastructure/DataStructureDemo.java
+++ design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/bridge/datastructure/DataStructureDemo.java
@@ -3,6 +3,12 @@ package com.eduardocode.objectorienteddesign.designpatterns.structural.bridge.da
 public class DataStructureDemo {
 
     public static void main(String[] args) {
+        FifoCollection<String> fifo = new Queue<>(new SinglyLinkedList<>());
+        fifo.offer("First");
+        fifo.offer("Second");
+        fifo.offer("Third");
 
+        System.out.println(fifo.poll());
+        System.out.println(fifo.poll());
     }
 }
--- design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/bridge/datastructure/Queue.java
+++ design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/bridge/datastructure/Queue.java
@@ -2,5 +2,20 @@ package com.eduardocode.objectorienteddesign.designpatterns.structural.bridge.da
 
 
 // refined abstraction
-public class Queue<T> {
+public class Queue<T> implements FifoCollection<T> {
+    private LinkedList<T> list;
+
+    public Queue(LinkedList<T> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void offer(T element) {
+        list.addLast(element);
+    }
+
+    @Override
+    public T poll() {
+        return list.removeFirst();
+    }
 }
