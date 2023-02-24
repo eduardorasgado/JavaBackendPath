a1974a2857e54db94241091875a8c8fd71717486
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-02-23 23:43:04 -0600

    adding file example skeleton for composite dp

---
+++ design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/composite/files/Directory.java
@@ -0,0 +1,4 @@
+package com.eduardocode.objectorienteddesign.designpatterns.structural.composite.files;
+
+public class Directory {
+}
---
+++ design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/composite/files/FilesDemo.java
@@ -0,0 +1,8 @@
+package com.eduardocode.objectorienteddesign.designpatterns.structural.composite.files;
+
+public class FilesDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
