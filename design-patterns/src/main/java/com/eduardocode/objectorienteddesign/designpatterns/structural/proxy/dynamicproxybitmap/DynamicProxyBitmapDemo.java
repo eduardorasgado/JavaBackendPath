f681c2807355fe26cd96a21921c5bce7329415d7
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-02-28 01:20:50 -0600

    an explanation of dynamic proxies was added to bitmap proxy example

--- design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/proxy/dynamicproxybitmap/DynamicProxyBitmapDemo.java
+++ design-patterns/src/main/java/com/eduardocode/objectorienteddesign/designpatterns/structural/proxy/dynamicproxybitmap/DynamicProxyBitmapDemo.java
@@ -2,8 +2,13 @@ package com.eduardocode.objectorienteddesign.designpatterns.structural.proxy.dyn
 
 public class DynamicProxyBitmapDemo {
 
+    /**
+     * Dynamic proxies are created using InvocationHandler from java reflect API
+     *
+     * The intention of the dynamic proxy is to allow us to create proxys for any object at runtime
+     * @param args
+     */
     public static void main(String[] args) {
-        // using dynamic proxy java API
         Image image = ImageFactory.getImage("myImage.png");
         image.setLocation(new Point2D(200, 1043));
         image.render();
