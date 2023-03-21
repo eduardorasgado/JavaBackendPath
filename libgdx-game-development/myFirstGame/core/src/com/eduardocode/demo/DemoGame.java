269d2ea3d7a8178c521929ef98ad39c512bdbd94
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-03-21 09:44:58 -0600

    camera and bucket sprinte added, also movement was added for bucket spritÂe

--- libgdx-game-development/myFirstGame/core/src/com/eduardocode/demo/DemoGame.java
+++ libgdx-game-development/myFirstGame/core/src/com/eduardocode/demo/DemoGame.java
@@ -4,7 +4,13 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class DemoGame extends ApplicationAdapter {
 
@@ -12,6 +18,12 @@ public class DemoGame extends ApplicationAdapter {
 	private Texture buckedImage;
 	private Sound dropSound;
 	private Music rainMusic;
+
+	private OrthographicCamera camera;
+
+	private SpriteBatch batch;
+
+	private Rectangle bucket;
 
 	@Override
 	public void create() {
@@ -23,5 +35,37 @@ public class DemoGame extends ApplicationAdapter {
 
 		rainMusic.setLooping(true);
 		rainMusic.play();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		batch = new SpriteBatch(2);
+
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+	}
+
+	@Override
+	public void render() {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
+
+		batch.begin();
+		batch.draw(buckedImage, bucket.x, bucket.y);
+		batch.end();
+
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+
+			camera.unproject(touchPos);
+
+			bucket.x = touchPos.x - 64 / 2;
+		}
 	}
 }
